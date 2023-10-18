a10afa7c8b887f39a98abfd6f3906be8bb5b5690
MrMaina100 <mburumaina648@gmail.com>
2023-10-18 21:25:45 +0300

    added a single listing page

---
+++ src/Pages/Listing.tsx
@@ -0,0 +1,11 @@
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../firebaseConfig"
+import { getAuth } from "firebase/auth"
+import { useParams } from "react-router-dom"
+
+const Listing = () => {
+  return (
+    <div>Listing</div>
+  )
+}
+export default Listing
