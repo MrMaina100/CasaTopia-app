7bbe5ac1b3861cb24f898d12d27c556834dadee9
MrMaina100 <mburumaina648@gmail.com>
2023-10-21 22:56:10 +0300

    style updates

--- src/Components/ListingItem.tsx
+++ src/Components/ListingItem.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import {TrashIcon} from '@radix-ui/react-icons'
 import { Card, CardSection } from "@mantine/core"
 import { DocumentData } from "firebase/firestore"
+import {SlLocationPin} from 'react-icons/sl'
 
 type propsTypes = {
    listing:DocumentData,
@@ -10,24 +11,30 @@ type propsTypes = {
 }
 const ListingItem = ({listing, id, onDelete}:propsTypes) => {
   return (
-    <div className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-5">
+    <div>
     
       <Link to={`/category/${listing.type}/${id}`}>
-         <Card withBorder radius={"md"} className="w-72">
-            <Card.Section className=" h-40">
-               <img src={listing.imageUrls[0]} alt="" />
+         <Card className="flex flex-col space-y-4 w-72  ">
+            <Card.Section className=" h-48">
+               <img src={listing.imageUrls[0]} alt="" className="w-full h-full"/>
             </Card.Section>
 
-            <Card.Section className="mt-10">
-               <p className="graylightText">
+            <div className="flex flex-col">
+             
+               <p className="graylightText flex items-center">
+                  <SlLocationPin/>
                   {listing.location}
                </p>
                <h1>{listing.name}</h1>
-            </Card.Section>
-            <CardSection>
-               <p>{listing.regularPrice}</p>
+             
+               
+             <div>
+               <p>{listing.regularPrice}{listing.type === 'rent'? '/per Month' : ''}
+               </p>
 
-            </CardSection>
+             </div>
+
+            </div>  
 
            
 
