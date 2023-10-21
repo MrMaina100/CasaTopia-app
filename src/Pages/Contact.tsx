209519f9d9190d2d9dfbeef9cac823e59a339c27
MrMaina100 <mburumaina648@gmail.com>
2023-10-21 11:51:01 +0300

    added a contant page for someone to contact the landlord

---
+++ src/Pages/Contact.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect, ChangeEvent} from "react"
+import { useParams, useSearchParams } from "react-router-dom"
+import { db } from "../firebaseConfig"
+import { doc,getDoc, DocumentData} from "firebase/firestore"
+import Button from "../Components/Button"
+
+const Contact = () => {
+   const [message, setMessage] = useState('')
+   const [landLord, setLandLord] = useState<DocumentData|null>(null)
+   const [searchParams, setSearchParams] = useSearchParams()
+   //this will allow us to get the listingName that we passing in as a query string
+
+   const params = useParams()
+   useEffect(()=>{
+
+      const getLandLord = async () => {
+         const docRef = doc(db, `users/${params.landLordId}`)
+         const docSnap = await  getDoc(docRef)
+
+         if(docSnap.exists()){
+            console.log(docSnap.data());
+            
+            setLandLord(docSnap.data())
+         }else{
+            console.log('landlord does not exists');
+            
+         }
+         
+      }
+
+      getLandLord()
+
+
+   },[params.landLordId])
+
+
+   const handleChange = (e:ChangeEvent<HTMLTextAreaElement>)=> setMessage(e.target.value)
+
+  return (
+    <div>
+      <p>Contact landlord</p>
+
+      {landLord !== null&&(
+         <div>
+            <p>{landLord.name}</p>
+            <form >
+               <label htmlFor="message">Message</label>
+               <textarea name="message" id="message" value={message} onChange={handleChange}>
+
+               </textarea>
+
+               <a href={`mailto:${landLord.email}?subject=${searchParams.get('listingName')}&body=${message}`}>
+                  <Button type="button">
+                     Send Message
+                  </Button>
+
+               </a>
+            </form>
+
+         </div>
+      )}
+    </div>
+  )
+}
+export default Contact
