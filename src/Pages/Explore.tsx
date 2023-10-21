230e68d5cffa74b38dc3a6c6c8521150133700ef
MrMaina100 <mburumaina648@gmail.com>
2023-10-21 12:00:31 +0300

    updates

--- src/Pages/Explore.tsx
+++ src/Pages/Explore.tsx
@@ -1,6 +1,26 @@
+import forSale from '../assets/forSell.jpg'
+import forRent from '../assets/forRent.jpg'
+import { Link } from 'react-router-dom'
+
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <div className='flex flex-col space-y-5'>
+      <h1>Category</h1>
+
+      <div className="flex  space-x-10">
+        <Link to='category/rent '>
+          <img src={forRent} alt="" className='w-48 ' />
+        </Link>
+
+          <Link to='category/sale'>
+            <img src={forSale} alt=""  className='w-48'/>
+        </Link>
+
+      </div>
+
+    </div>
+    
+    
   )
 }
 export default Explore
